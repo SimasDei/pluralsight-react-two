310dfad77565796ef9a503825598b02c3012f7b2
SimasDei <simondeiman@gmail.com>
2018-12-23 00:05:10 +0200

    add book and author components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function Hero() {
+  return (
+    <div className="row">
+      <div className="jumbotron col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>Select the book, written by the Displayed Author</p>
+      </div>
+    </div>
+  );
+}
+
+function Turn({ author, books }) {
+  return (
+    <div className="row turn">
+      <div className="col-4 offset-1">
+        <img src={author.imageUrl} className="authorimage" alt="Author" />
+      </div>
+      <div className="col-6">
+        {books.map(title => (
+          <p>{title}}</p>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Continue() {
+  return <div />;
+}
+
+function Footer() {
+  return (
+    <div className="row">
+      <div className="col-12">
+        <p className="text-muted credit">
+          Images from{' '}
+          <a href="https://commons.wikimedia.org/">Wikimedia Commons</a>
+        </p>
+      </div>
+    </div>
+  );
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container-fluid">
+        <Hero />
+        <Turn />
+        <Continue />
+        <Footer />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const authors = [
+  {
+    name: 'Mark Twain',
+    imageUrl: 'images/authors/marktwain.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['The Adventures of Huckleberry Finn']
+  }
+];
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
